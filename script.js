12ee9335e9660ec8f8bf7152a7285b65301848dd
Damian Bruno <damianbruno17@gmail.com>
2021-09-20 21:46:40 -0300

    "Added a GUI to the game"

--- script.js
+++ script.js
@@ -1,62 +1,68 @@
-"use script"
+'use strict'
+
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const actionMessage = document.getElementById('message');
+const rockDiv = document.getElementById('rock');
+const paperDiv = document.getElementById('paper');
+const scissorsDiv = document.getElementById('scissors');
+
 
 function computerPlay() {
-	let opciones = ['rock', 'paper', 'scissors'];
-	return opciones[Math.floor(Math.random() * 3)];
+  const options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * 3)];
+};
+
+function game(userChoice) {
+  const computerChoice = computerPlay();
+
+  switch (userChoice + computerChoice) {
+    case 'rockscissors':
+    case 'paperrock':
+    case 'scissorspaper':
+      win(userChoice, computerChoice);
+      break;
+    case 'rockpaper':
+    case 'paperscissors':
+    case 'scissorsrock':
+      lose(userChoice, computerChoice);
+      break;
+    case 'rockrock':
+    case 'paperpaper':
+    case 'scissorsscissors':
+      draw(userChoice, computerChoice);
+      break;
+  };
+};
+
+function win(userChoice, computerChoice) {
+  const userChoiceDiv = document.getElementById(userChoice);
+  userScore++;
+  userScore_span.textContent = userScore;
+  actionMessage.textContent = `You Win! ${userChoice} beats ${computerChoice}`;
+  userChoiceDiv.classList.add('greenGlow');
+  setTimeout( () => userChoiceDiv.classList.remove('greenGlow'), 300);
+};
+function lose(userChoice, computerChoice) {
+  const userChoiceDiv = document.getElementById(userChoice);
+  computerScore++;
+  computerScore_span.textContent = computerScore;
+  actionMessage.textContent = `You Loose! ${computerChoice} beats ${userChoice}`
+  userChoiceDiv.classList.add('redGlow');
+  setTimeout( () => userChoiceDiv.classList.remove('redGlow'), 300);
+};
+function draw (userChoice, computerChoice) {
+  const userChoiceDiv = document.getElementById(userChoice);
+  actionMessage.textContent = `It's a Draw! both pick ${userChoice}`;
+  userChoiceDiv.classList.add('greyGlow');
+  setTimeout( () => userChoiceDiv.classList.remove('greyGlow'), 300);
 }
+function main() {
+  rockDiv.addEventListener('click', () => game('rock'));
+  paperDiv.addEventListener('click', () => game('paper'));
+  scissorsDiv.addEventListener('click', () => game('scissors'));
+};
 
-function humanPlay() {
-	let seleccion = prompt('Choose your weapon', '').toLowerCase();
-	if (seleccion === 'rock' ||
-		seleccion === 'paper' ||
-		seleccion === 'scissors') {
-		return seleccion;
-	}else {
-		alert('Invalid choise, pick again!');
-		return humanPlay();
-	}
-}
-
-function playRound(computerSelection, humanSelection) {
-	if (humanSelection === computerSelection) {
-		return `It's a tie! Both pick ${humanSelection.toUpperCase()}`;
-	}else if (
-		humanSelection === 'rock' && computerSelection === 'scissors' ||
-		humanSelection === 'paper' && computerSelection === 'rock' ||
-		humanSelection === 'scissors' && computerSelection === 'paper'
-		) {
-		humanScore++;
-		return `Player Wins! ${humanSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
-	}else {
-		computerScore++;
-		return `Bad Luck, Computer Wins! ${computerSelection.toUpperCase()} beats ${humanSelection.toUpperCase()}`;
-	}
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-function keepScore(humanScore, computerScore) {
-	if (humanScore > computerScore) {
-		console.log(`Player Wins the Game!!\nThe final score is Player:${humanScore} - Computer:${computerScore}`);
-	}else if(computerScore > humanScore) {
-		console.log(`Computer Wins the Game!!\nThe final score is Computer:${computerScore} - Player:${humanScore}`);
-	}else {
-		console.log(`Tie Game!\nThe final score is Player:${humanScore} - Computer:${computerScore} `);
-	};
-}
-
-
-function game() {
-
-	for (let i = 0; i < 5; i++) {
-		const humanSelection = humanPlay();
-		const computerSelection = computerPlay();
-
-		console.log(playRound(computerSelection, humanSelection));
-	}
-	return keepScore(humanScore, computerScore);
-	
-}
-
-game();
+main();
